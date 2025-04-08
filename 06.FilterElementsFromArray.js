50df9fd095d840199bfcf8b80a7121beb6526f34
md-zeon <zeonrahaman5870@gmail.com>
2025-04-08 18:38:19 +0600

    Added an Example

--- 06.FilterElementsFromArray.js
+++ 06.FilterElementsFromArray.js
@@ -14,3 +14,8 @@ var filter = function(arr, fn) {
 
     return result;
 };
+
+const arr = [10, 20, 30, 40];
+const fn = (n, i) => n > 25 && i % 2 === 0;
+
+console.log(filter(arr, fn)); // Output: [30]
